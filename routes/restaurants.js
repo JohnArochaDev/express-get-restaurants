a4afa2b4e2dbe7eaf266eb34d35bbe050a223200
John Arocha <john.arocha@clover.com>
2024-11-12 15:13:12 -0700

    added new middleware function to post

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,6 +2,8 @@ const express = require('express')
 const restaurantRoutes = express.Router()
 const Restaurant = require('../models/index.js')
 
+const { check, validationResult } = require('express-validator');
+
 restaurantRoutes.get('/', async (req, res) => {
     const restaurants = await Restaurant.findAll()
     res.status(200).json(restaurants)
@@ -14,11 +16,24 @@ restaurantRoutes.get('/:id', async (req, res) => {
     res.status(200).json(restaurant)
 })
 
-restaurantRoutes.post('/', async (req, res) => {
-    const restaurantData = req.body
-    const newRestaurant = await Restaurant.create(restaurantData)
-    res.status(201).json(newRestaurant)
-})
+restaurantRoutes.post(
+    '/',
+    [
+        check('name').trim().notEmpty().withMessage('Name is required and cannot be empty or whitespace'),
+        check('location').trim().notEmpty().withMessage('Location is required and cannot be empty or whitespace'),
+        check('cuisine').trim().notEmpty().withMessage('Cuisine is required and cannot be empty or whitespace')
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const restaurantData = req.body;
+        const newRestaurant = await Restaurant.create(restaurantData);
+        res.status(201).json(newRestaurant);
+    }
+);
 
 restaurantRoutes.put('/:id', async (req, res) => {
     const id = req.params.id;
